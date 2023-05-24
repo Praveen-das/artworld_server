b75a5c514a7660032ba2e2adeb85ad45b1cdcc03
praveen das <praveendask97@gmail.com>
2023-05-24 19:01:04 +0530

    setcookie

--- api/product.ts
+++ api/product.ts
@@ -3,10 +3,9 @@ const router = express.Router();
 
 router.get("/", async (req: any, res: any) => {
   try {
-    res.json({
-      status: 200,
-      message: "route working successfully",
-    });
+    res.setHeader('Set-Cookie', ['name=praveen']);
+    res.send("route working successfully");
+    // res.end('New cookie set');
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
