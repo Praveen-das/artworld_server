e1debe8d38b1584c52a5b549a4711155cc10f43e
Praveen-das <praveendask97@gmail.com>
2024-01-22 05:37:07 +0530

    220124

--- api/config/prismaClient.ts
+++ api/config/prismaClient.ts
@@ -1,4 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { tmpdir } from 'os'
+
+import fs from 'fs'
+
+fs.writeFile(
+    `${tmpdir()}/ca.pem`,
+    process.env.DATABASE_URL!,
+    err => {
+        if (err) return console.log(err)
+    }
+)
+
 const client = new PrismaClient();
 
 export default client
