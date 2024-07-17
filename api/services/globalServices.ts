acfa13fb344af14a7f5eeb8c4a14dcfee4647b09
praveen das <praveendask97@gmail.com>
2024-07-17 10:51:56 +0530

    asdasdasd

--- api/services/globalServices.ts
+++ api/services/globalServices.ts
@@ -1,4 +1,6 @@
 const auth = (req: any, res: any, next: any) => {
+  console.log(req.isAuthenticated());
+  
   if (req.isAuthenticated()) return next();
   next({ message: "Unautherized", code: 401 });
 };
