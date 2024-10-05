10d373ab383cf3ffedb8c0e787cb68d0ff5d2704
praveen das <praveendask97@gmail.com>
2024-10-05 17:08:00 +0530

    test6

--- api/test/test.ts
+++ api/test/test.ts
@@ -13,4 +13,16 @@ router.get("/", async (req: any, res: any) => {
   }
 });
 
+router.get("/data", async (req: any, res: any) => {
+  try {
+    res.json({
+      status: 200,
+      message: "get route data successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
+
 export default router;
